7cd67a49a25ff1b965b9148879d66cbf30f5a4c6
Chee Sam Leong <cheesam@codedsphere.com>
2021-03-07 02:28:11 +0800

    Added tokenization

--- hello_world.py
+++ hello_world.py
@@ -16,3 +16,4 @@ def function2():
 def authorization():
     print("Added authorization function")
     print("Bug fix in authorization function 2")
+    print("Added more handling on tokenization when it comes to authorization")
