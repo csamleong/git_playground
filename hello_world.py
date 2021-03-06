c7acc29b0661bf26e581f2ab6211b288cbb44238
Chee Sam Leong <cheesam@codedsphere.com>
2021-03-07 02:20:20 +0800

    Added function2

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,12 @@
 print("hello world")
 
 
-def function_feature1():
-    print("Calling from feature1 function")
+def function1():
+    print("Calling from function")
 
 
-function_feature1()
+function1()
+
+
+def function2():
+    print("Added function2")
